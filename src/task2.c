ae39277ad3a0c026bf6d8b6e94b2d4917a8e0f4d
Vasea <zzzargoman@gmail.com>
2025-01-13 21:36:51 +0200

    85p Completed the ecnryption task. However, I used a global variable pretty much everywhere. 5min and I'll adapt it so that I won't use any global non-const variables.
    Signed-off-by: Vasea <zzzargoman@gmail.com>

--- src/task2.c
+++ src/task2.c
@@ -3,23 +3,6 @@
 #include <stdarg.h>
 #include <math.h>
 
-#define MAX_COMMAND_LENGTH 100
-#define MAX_COMAMND_TYPE_LENGTH 10
-#define MAX_COLUMN_NAME_LENGTH 15
-#define STUDENTS_TABLE_COLUMNS 5
-#define COURSES_TABLE_COLUMNS 3
-#define ENROLLMENTS_TABLE_COLUMNS 3
-#define MAX_TABLE_NAME_LENGTH 10
-#define MAX_CONDITION_LENGTH 60
-#define MAX_OPERATOR_LENGTH 3
-#define MAX_VALUE_LENGTH 40
-#define STUDENTS_TABLE 0
-#define COURSES_TABLE 1
-#define ENROLLMENTS_TABLE 2
-#define MAX_GRADE_LENGTH 6
-#define GRADE_COUNT 3
-#define EPSILON 0.000001
-
 // #define DEBUG
 
 bool check_student(char *condition_column, char *condition_operator, char *condition_value, student *slave) {
--- src/task2.h
+++ src/task2.h
@@ -3,5 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_COMMAND_LENGTH 100
+#define MAX_COMAMND_TYPE_LENGTH 10
+#define MAX_COLUMN_NAME_LENGTH 15
+#define STUDENTS_TABLE_COLUMNS 5
+#define COURSES_TABLE_COLUMNS 3
+#define ENROLLMENTS_TABLE_COLUMNS 3
+#define MAX_TABLE_NAME_LENGTH 10
+#define MAX_CONDITION_LENGTH 60
+#define MAX_OPERATOR_LENGTH 3
+#define MAX_VALUE_LENGTH 40
+#define STUDENTS_TABLE 0
+#define COURSES_TABLE 1
+#define ENROLLMENTS_TABLE 2
+#define MAX_GRADE_LENGTH 6
+#define GRADE_COUNT 3
+#define EPSILON 0.000001
+
 #include "task1.h"
 #include "../include/structuri.h"
+
